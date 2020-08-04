6a3a6cbf0cd9eb50896b3f248964798a0bba97c4
justinhjy1004 <46717137+justinhjy1004@users.noreply.github.com>
2020-08-04 14:51:36 -0500

    Floyd Warshall added comments

--- Assignment 3/FloydWarshall.py
+++ Assignment 3/FloydWarshall.py
@@ -1,23 +1,38 @@
+# Abby Seibel and Justin Ho
+# CSCE310
+# Assignment 3
+# Graph Algorithms
+# Floyd-Warshall Shortest Path Algorithm
+
 import graph_representation as gr
 
+'''
+Given graph, sets up an adjacency matrix of the given graph
+'''
 def matrix_setup(graph:gr.Graph):
     n = graph.num_vertices()
     adj_matrix = []
+    # Each element in matrix is initialized with empty list
     for i in range(n):
         adj_matrix.append([])
         for j in range(n):
             adj_matrix[i].append([])
 
+    # diagonal of matrix initialized with edge to self
     for i in range(n):
         e = gr.Edge(graph.get_node(i),graph.get_node(i),0)
         adj_matrix[i][i].append(e)
 
+    # direct paths are initialized
     for e in graph.get_edge_list():
         adj_matrix[e.get_nodes()[0].get_value()][e.get_nodes()[1].get_value()].append(e)
         adj_matrix[e.get_nodes()[1].get_value()][e.get_nodes()[0].get_value()].append(e)
 
     return adj_matrix
 
+'''
+Print output of the all shortest paths solution
+'''
 def build_path(adj_matrix, graph:gr.Graph):
     n = len(adj_matrix)
 
@@ -43,14 +58,18 @@ def build_path(adj_matrix, graph:gr.Graph):
                 print("Path weight: %.2f" % (total_weight))
                 print()
 
-
+'''
+Implementation of Floyd-Warshall Algorithm using an adjacency matrix and improving 
+the path given graph
+'''
 def floyd_warshall(adj_matrix, graph:gr.Graph):
-    n =  len(adj_matrix)
+
+    n = len(adj_matrix)
 
     for k in range(n):
         for i in range(n):
             for j in range(n):
-
+                # consider first path
                 path1 = None
                 if len(adj_matrix[i][k]) == 0:
                     if graph.get_edge(i,k) is not None:
@@ -64,6 +83,7 @@ def floyd_warshall(adj_matrix, graph:gr.Graph):
                     for e in adj_matrix[i][k]:
                         weight1 = weight1 + e.get_weight()
 
+                # consider second path
                 path2 = None
                 if len(adj_matrix[k][j]) == 0:
                     if graph.get_edge(k,j) is not None:
@@ -77,10 +97,12 @@ def floyd_warshall(adj_matrix, graph:gr.Graph):
                     for e in adj_matrix[k][j]:
                         weight2 = weight2 + e.get_weight()
 
+                # original weight
                 og_weight = 0
                 for e in adj_matrix[i][j]:
                     og_weight = og_weight + e.get_weight()
 
+                # update path if better path found
                 if weight1 and weight2 is not None:
                     weight = weight1 + weight2
                     if isinstance(path1, gr.Edge) and isinstance(path2, gr.Edge):
